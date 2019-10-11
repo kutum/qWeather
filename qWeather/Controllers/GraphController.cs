f2567221940d3efbfa0f635010fb92cf73d84f4c
drago <dragono0146@gmail.com>
2019-10-11 21:30:06 +0400

    add api requests

--- qWeather/Controllers/GraphController.cs
+++ qWeather/Controllers/GraphController.cs
@@ -1,5 +1,6 @@
 ﻿using qWeather.Context;
 using qWeather.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -11,25 +12,84 @@ namespace qWeather.Controllers
     [RoutePrefix("api/graph")]
     public class MyApiController : ApiController
     {
+        public List<Weather> weather = new List<Weather>();
+        public List<WeatherView> weatherViews = new List<WeatherView>();
+
         // GET: Weather
         [Route("")]
         [HttpGet]
         public IEnumerable<WeatherView> Get()
         {
-            List<Weather> weather = new List<Weather>();
-
             using (var context = new WeatherDbContext())
             {
                 weather = context.Weather.OrderByDescending(x => x.DATETIME).ToList();
             }
-
-            List<WeatherView> weatherViews = new List<WeatherView>();
 
             foreach (var item in weather)
             {
                 weatherViews.Add(new WeatherView(item));
             }
             return weatherViews;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<WeatherView> Get([FromUri]DateTime start, [FromUri]DateTime end)
+        {
+            using (var context = new WeatherDbContext())
+            {
+                weather = context.Weather.Where(x=>x.DATETIME>=start && x.DATETIME<=end).OrderByDescending(x => x.DATETIME).ToList();
+            }
+
+            foreach (var item in weather)
+            {
+                weatherViews.Add(new WeatherView(item));
+            }
+            return weatherViews;
+        }
+
+        [Route("last")]
+        [HttpGet]
+        public IEnumerable<WeatherView> GetLast()
+        {
+            using (var context = new WeatherDbContext())
+            {
+                weather = context.Weather.Where(x => x.DATETIME == context.Weather.Max(y=>y.DATETIME)).OrderByDescending(x => x.DATETIME).ToList();
+            }
+
+            foreach (var item in weather)
+            {
+                weatherViews.Add(new WeatherView(item));
+            }
+            return weatherViews;
+        }
+        [Route("average")]
+        [HttpGet]
+        public WeatherAverageView GetAverage([FromUri]DateTime start, [FromUri]DateTime end)
+        {
+            
+            using (var context = new WeatherDbContext())
+            {
+                weather = context.Weather.Where(x => x.DATETIME >= start && x.DATETIME <= end).ToList();
+            }
+
+            float insideSum = 0.0f;
+            float outsideSum = 0.0f;
+
+            foreach (var item in weather)
+            {
+                insideSum += item.VAL1;
+                outsideSum += item.VAL2;
+            }
+
+            WeatherAverageView weatherAverageView = new WeatherAverageView(){
+                datetimefrom = start.ToString("yyyy-MM-dd HH:mm:ss"),
+                datetimeend = end.ToString("yyyy-MM-dd HH:mm:ss"),
+                insideTemp = insideSum / weather.Count,
+                outsideTemp = outsideSum / weather.Count
+            };
+
+            return weatherAverageView;
         }
     }
 }
---
+++ qWeather/Models/WeatherAverageView.cs
@@ -0,0 +1,12 @@
+﻿namespace qWeather.Models
+{
+    public class WeatherAverageView
+    {
+        public string datetimefrom { get; set; }
+        public string datetimeend { get; set; }
+        public float outsideTemp { get; set; }
+        public float insideTemp { get; set; }
+
+        public WeatherAverageView() { }
+    }
+}
